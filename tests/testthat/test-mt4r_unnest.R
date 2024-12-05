21c5e91e2c260dbf526a4ffcf744b0219acd0ab5
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-12-05 11:07:35 +0200

    add first unit tests

---
+++ tests/testthat/test-mt4r_unnest.R
@@ -0,0 +1,13 @@
+df <- data.frame(
+  x = c(1, 2, 3),
+  y = c(4, 5, 6)
+)
+
+test_that("incorrectly formatted data.frame errors", {
+  expect_error(mt4r_unnest(df))
+})
+
+
+test_that("non-data.frame produces an error", {
+  expect_error(mt4r_unnest(1:4))
+})
