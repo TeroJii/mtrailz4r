4d1372ea7899a012120492cbe466b4df33ab2ea3
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-12-12 16:22:35 +0200

    add roxygen comments for documentation

--- R/mt4r_fixtime.R
+++ R/mt4r_fixtime.R
@@ -1,3 +1,20 @@
+#' Fix the timestamp column in the data
+#'
+#' The `event_timestamp` column in the data is a character vector, which
+#' represents time passed since the unix epoch in microseconds. This function
+#' converts the `event_timestamp` column into a datetime object.
+#'
+#' @param dat A data frame with the following columns: event_timestamp,
+#' event_params.value.int_value, event_params.value.double_value. The input
+#' data.frame is created by the [mt4r_unnest()] and optionally modified by the
+#' [mt4r_addsessionid()] function.
+#'
+#' @return A data frame with the `event_timestamp` column converted into a
+#'  datetime object (`event_timestamp2`).
+#' @export
+#'
+#' @examples
+#' # To be added
 mt4r_fixtime <- function(dat){
 
   stopifnot(is.data.frame(dat))
@@ -20,6 +37,7 @@ mt4r_fixtime <- function(dat){
     ) |>
     dplyr::mutate(
       event_timestamp2 = lubridate::as_datetime(
+        # timestamp is microseconds since the unix epoch
         event_timestamp / (1000*1000)
         ),
       .after = event_timestamp
