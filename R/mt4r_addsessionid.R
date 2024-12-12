4a5be3fd3a7d864745b89a77c5cd3a8f04524129
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-12-12 14:19:22 +0200

    add Roxygen comments to function

--- R/mt4r_addsessionid.R
+++ R/mt4r_addsessionid.R
@@ -1,3 +1,17 @@
+#' Add session_id column
+#'
+#' This function adds a session_id column to the data.
+#' The session_id is a unique identifier for each session.
+#'
+#' @param dat A data frame with the following columns: event_params.key,
+#' event_date, event_params.value.int_value, row_id. The input data.frame is
+#' created by the [mt4r_unnest()] function.
+#'
+#' @return A data frame with the session_id column added.
+#' @export
+#'
+#' @examples
+#' # To be added
 mt4r_addsessionid <- function(dat){
 
   stopifnot(is.data.frame(dat))
@@ -25,3 +39,6 @@ mt4r_addsessionid <- function(dat){
 
   return(has_session_id)
 }
+
+# Supress "Undefined global functions or variables" R CMD check note
+event_params.key <- event_params.value.int_value <- row_id <- session_id <- NULL
