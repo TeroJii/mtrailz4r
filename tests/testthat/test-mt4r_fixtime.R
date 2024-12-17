99a27c1e7c6d4505f7f98451b4f0a0d44bc1f3d5
TeroJii <78660782+TeroJii@users.noreply.github.com>
2024-12-17 15:28:53 +0200

    add new unit tests

--- tests/testthat/test-mt4r_fixtime.R
+++ tests/testthat/test-mt4r_fixtime.R
@@ -11,3 +11,34 @@ test_that("incorrectly formatted data.frame errors", {
 test_that("non-data.frame produces an error", {
   expect_error(mt4r_fixtime(1:4))
 })
+
+test_that("correctly formatted data.frame does not error", {
+  expect_silent(object = {
+    mockdata |>
+      mt4r_unnest() |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime()
+  })
+})
+
+
+test_that("correctly formatted data.frame producess new timestamp column", {
+  expect_true(object = {
+    test_dat <- mockdata |>
+      mt4r_unnest() |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime()
+
+    "event_timestamp2" %in% colnames(test_dat)
+  })
+})
+
+test_that("correctly formatted data.frame has correct dimensions", {
+  expect_equal(
+    mt4r_unnest(mockdata) |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime() |>
+      dim(),
+    c(357, 12)
+  )
+})
