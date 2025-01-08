ad478d0b42ba632f2e0a6bafd3abeced937c2710
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-01-08 14:42:59 +0200

    add assertive tests

--- R/plot_popular.R
+++ R/plot_popular.R
@@ -1,11 +1,19 @@
 plot_popular <- function(dat, x_var, y_var, col_fill = "#99ff99",
                          col_color = "gray", alpha_val = 0.9){
 
+  stopifnot(is.data.frame(dat))
+  stopifnot(is.character(x_var) && is.character(y_var) &&
+              is.character(col_fill) &&is.character(col_color))
+  stopifnot(is.numeric(alpha_val))
+  stopifnot(x_var %in% names(dat))
+  stopifnot(y_var %in% names(dat))
+  stopifnot(alpha_val >= 0 && alpha_val <= 1)
+
   dat |>
     ggplot2::ggplot(
       ggplot2::aes(
         x = {{x_var}},
-        y = reorder({{y_var}}, {{x_var}})
+        y = stats::reorder({{y_var}}, {{x_var}})
       )
     ) +
     ggplot2::geom_col(
