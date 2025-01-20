e451be6230636e34df0f3c9f84c7e4ae1b465bbe
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-01-20 16:37:33 +0200

    add unit tests with simple test data

--- tests/testthat/test-mt4r_routestarts.R
+++ tests/testthat/test-mt4r_routestarts.R
@@ -74,3 +74,31 @@ test_that("lookup data contains same route start numbers as output data", {
     unique(lookup_dat$route_started_number)
   )
 })
+
+
+test_that("test data returns sane outputs", {
+  test_data <- data.frame(
+    row_id = 1:11,
+    event_name = c("route_started", "route_stopped", "wut", "route_started", "route_stopped",
+                   "route_started", "route_stopped", "route_started", "route_stopped",
+                   "route_started", "route_stopped"),
+    event_params.key = c(rep("route_name", 2), "", rep("route_name", 8)),
+    event_params.value.string_value = c("A", "A", "A", "B", "B", "C", "C", "D", "D", "E", "E"),
+    session_id = c(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+  )
+
+  lookup_dat <- test_data |>
+    mt4r_routelookup()
+
+  expect_s3_class(test_data |>
+                    mt4r_routestarts(route_lookup = lookup_dat),
+                  "tbl_df"
+  )
+  # route starts with test data equal the number of route_started tags
+  expect_equal(
+    test_data |>
+      mt4r_routestarts(route_lookup = lookup_dat) |>
+      dplyr::pull(route_started_number) |>
+      max(),
+    sum(test_data$event_name == "route_started"))
+})
