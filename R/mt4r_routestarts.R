f33c74b43cbbfa3147be1035a4d4619663073f38
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-01-20 15:57:35 +0200

    add roxygen skeleton

--- R/mt4r_routestarts.R
+++ R/mt4r_routestarts.R
@@ -1,3 +1,33 @@
+#' Add route name and route start numbers to data
+#'
+#' This function adds a running route start number to the data. Additionally,
+#' a `route_name` column is added to the data, to help easily connect route
+#' starts to specific routes.
+#'
+#' @param dat A data frame with the following columns: row_id, session_id,
+#' event_name, event_params.key, event_params.value.string_value. The input
+#' data.frame is created by the [mt4r_unnest()], [mt4r_fixtime()], and
+#' [mt4r_addsessionid()] functions.
+#' @param route_lookup A lookup  data frame with the running route start
+#' numbers. Is created by the [mt4r_routelookup()] function.
+#' @param filter_zero_data A logical value. If TRUE, rows where a route has not
+#' started are removed from the data.
+#'
+#' @returns A data frame with the `route_started_number` and `route_name`
+#' columns added to the input data.
+#' @export
+#'
+#' @examples
+#' dat <- mockdata |>
+#'   mt4r_unnest() |>
+#'   mt4r_fixtime() |>
+#'   mt4r_addsessionid()
+#'
+#'  lookup_dat <- dat |>
+#'   mt4r_routelookup()
+#'
+#'  dat |>
+#'    mt4r_routestarts(lookup_dat)
 mt4r_routestarts <- function(dat, route_lookup, filter_zero_data = TRUE){
 
   stopifnot(is.data.frame(dat))
@@ -31,3 +61,6 @@ mt4r_routestarts <- function(dat, route_lookup, filter_zero_data = TRUE){
 
   return(routestart_dat)
 }
+
+# Suppress "Undefined global functions or variables" R CMD check note
+route_started_number <- event_params.value.string_value <- route_name <-  NULL
