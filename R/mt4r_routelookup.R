bbbd70162f9aa986d0cad743f4e390c33d7f622b
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-01-20 14:54:22 +0200

    add assertive tests

--- R/mt4r_routelookup.R
+++ R/mt4r_routelookup.R
@@ -1,4 +1,10 @@
 mt4r_routelookup <- function(dat){
+
+  stopifnot(is.data.frame(dat))
+  stopifnot("row_id" %in% names(dat))
+  stopifnot("event_name" %in% names(dat))
+  stopifnot("event_timestamp2" %in% names(dat))
+
   lookup_dat <- dat |>
     dplyr::group_by(row_id) |>
     dplyr::mutate(
