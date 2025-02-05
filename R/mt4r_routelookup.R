b6a8aea0385312dd7222d9a3b515f3ead46c8e5a
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-02-05 14:09:49 +0200

    remove commented out code

--- R/mt4r_routelookup.R
+++ R/mt4r_routelookup.R
@@ -28,7 +28,6 @@ mt4r_routelookup <- function(dat){
 
   lookup_dat <- dat |>
     dplyr::group_by(row_id, session_id, event_name, event_timestamp2) |>
-    # dplyr::group_by(row_id) |>
     dplyr::mutate(has_route_started = any(event_name == "route_started")) |>
     dplyr::summarise(
       has_route_started = mean(has_route_started)
@@ -36,7 +35,6 @@ mt4r_routelookup <- function(dat){
     dplyr::ungroup() |>
     dplyr::arrange(session_id, event_timestamp2) |>
     dplyr::mutate(route_started_number = cumsum(has_route_started)) |>
-    # dplyr::ungroup()
     dplyr::select(row_id, route_started_number)
 
   return(lookup_dat)
