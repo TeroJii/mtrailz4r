76e06f934d301b38ed86be8f1f30337f8b748fc2
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-02-13 11:50:49 +0200

    add roxygen skeleton for documentation

--- R/mt4r_stoptagvals.R
+++ R/mt4r_stoptagvals.R
@@ -1,3 +1,23 @@
+#' Get tag values for each route stop
+#'
+#' Gets the numeric values for route_id, engagement_time, and route_pois_finished
+#' related to each unique route_stopped event.
+#'
+#' @param dat Input data.frame containing the columns row_id, event_name,
+#' event_params.key, event_params.value.int_value, and
+#' event_params.value.double_value. The input data is created with the functions
+#' [mt4r_unnest()], [mt4r_addsessionid()], and [mt4r_fixtime()].
+#'
+#' @returns A data.frame with the columns row_id, session_id, event_name,
+#' route_id, engagement_time (in seconds), and route_pois_finished (percentage).
+#' @export
+#'
+#' @examples
+#' mockdata |>
+#'   mt4r_unnest() |>
+#'   mt4r_addsessionid() |>
+#'   mt4r_fixtime() |>
+#'   mt4r_stoptagvals()
 mt4r_stoptagvals <- function(dat){
   stopifnot(is.data.frame(dat))
   stopifnot("event_name" %in% colnames(dat))
