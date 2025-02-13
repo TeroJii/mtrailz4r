c22b2e832643fa03a878582394659a1c73213c49
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-02-13 11:43:22 +0200

    assert that proper column names are found from input data

--- R/mt4r_stoptagvals.R
+++ R/mt4r_stoptagvals.R
@@ -1,5 +1,10 @@
 mt4r_stoptagvals <- function(dat){
   stopifnot(is.data.frame(dat))
+  stopifnot("event_name" %in% colnames(dat))
+  stopifnot("row_id" %in% colnames(dat))
+  stopifnot("event_params.key" %in% colnames(dat))
+  stopifnot("event_params.value.int_value" %in% colnames(dat))
+  stopifnot("event_params.value.double_value" %in% colnames(dat))
 
 
   # get the numeric values for route_id, engagement_time, and route_pois_finished
