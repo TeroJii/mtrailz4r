8e7baf0a583d9078236eac712b26fdec403d3fbd
TeroJii <78660782+TeroJii@users.noreply.github.com>
2025-02-13 11:43:52 +0200

    add tests to check that function errors with improper input

--- tests/testthat/test-mt4r_stoptagvals.R
+++ tests/testthat/test-mt4r_stoptagvals.R
@@ -16,3 +16,50 @@ test_that("that output is a data.frame", {
     },
     class = "data.frame")
 })
+
+
+test_that("function throws error if correct column names are not present", {
+  expect_error(
+    mockdata |>
+      mt4r_unnest() |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime() |>
+      dplyr::select(-row_id) |>
+      mt4r_stoptagvals()
+  )
+  expect_error(
+    mockdata |>
+      mt4r_unnest() |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime() |>
+      dplyr::select(-event_name) |>
+      mt4r_stoptagvals()
+  )
+  expect_error(
+    mockdata |>
+      mt4r_unnest() |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime() |>
+      dplyr::select(-event_params.key) |>
+      mt4r_stoptagvals()
+  )
+  expect_error(
+    mockdata |>
+      mt4r_unnest() |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime() |>
+      dplyr::select(-event_params.value.int_value) |>
+      mt4r_stoptagvals()
+  )
+  expect_error(
+    mockdata |>
+      mt4r_unnest() |>
+      mt4r_addsessionid() |>
+      mt4r_fixtime() |>
+      dplyr::select(-event_params.value.double_value) |>
+      mt4r_stoptagvals()
+  )
+  expect_error(
+    mtcars |> mt4r_stoptagvals()
+  )
+})
